3690e6b8138fbfba7c65ee5d6976e135f06b7e13
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-03-31 17:41:41 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    row = str(input())
+    num_list = [int(row[i:i+1]) for i in range(len(row))]
+    graph.append(num_list)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    
+    while queue:
+        x, y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+                
+            if graph[nx][ny] == 0:
+                continue
+            
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+                
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
