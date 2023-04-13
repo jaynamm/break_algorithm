c24ab79935ee67cc145acab2ce87fb6f1febd7ec
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-04-13 23:18:25 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -1,9 +1,2 @@
 def solution(array):
-    answer = 0
-    
-    for arr in array:
-        for a in str(arr):
-            if "7" in a:
-                answer += 1
-    
-    return answer
+    return str(array).count('7')
