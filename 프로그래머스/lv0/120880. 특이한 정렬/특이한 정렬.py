aac22602c7c19fc9739189e16e4080242307ac66
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-04-17 10:01:27 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,19 +1,22 @@
 def solution(numlist, n):
     answer = []
-    gap = []
+#     gap = []
     
-    for num in numlist:
-        gap.append([num, abs(n-num)])
+#     for num in numlist:
+#         gap.append([num, abs(n-num)])
     
-    gap.sort(key = lambda x:(x[1], x[0]))
+#     gap.sort(key = lambda x:(x[1], x[0]))
     
-    answer.append(gap[0][0])
+#     answer.append(gap[0][0])
     
-    for i in range(1, len(gap)):
-        if gap[i-1][1] == gap[i][1]:
-            answer[i-1] = gap[i][0]
-            answer.append(gap[i-1][0])
-        else:
-            answer.append(gap[i][0])
+#     for i in range(1, len(gap)):
+#         if gap[i-1][1] == gap[i][1]:
+#             answer[i-1] = gap[i][0]
+#             answer.append(gap[i-1][0])
+#         else:
+#             answer.append(gap[i][0])
+
+    numlist.sort(key = lambda x :(abs(x-n), n-x))
     
-    return answer
+    # return answer
+    return numlist
