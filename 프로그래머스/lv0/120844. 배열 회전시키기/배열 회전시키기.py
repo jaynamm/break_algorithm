f4c42ffcb382e0bb94d114a281bcfe4e7cb7b5f1
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-04-30 18:25:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,11 +1,2 @@
 def solution(numbers, direction):
-    answer = []
-    
-    if direction == "left":
-        answer += numbers[1:]
-        answer.append(numbers[0])
-    elif direction == "right":
-        answer.append(numbers[-1])
-        answer += numbers[:-1]
-    
-    return answer
+    return numbers[1:] + [numbers[0]] if direction == "left" else [numbers[-1]] + numbers[:-1] 
