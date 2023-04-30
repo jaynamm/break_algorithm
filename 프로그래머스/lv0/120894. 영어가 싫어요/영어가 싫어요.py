68156c907cd6ede05af358e1cd3d2a9faae44b71
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-04-30 19:10:18 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -1,8 +1,5 @@
 def solution(numbers):
-    alpha = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    
-    for i in range(len(alpha)):
-        numbers = numbers.replace(alpha[i], number[i])
+    for i, a in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(a, str(i))
     
     return int(numbers)
