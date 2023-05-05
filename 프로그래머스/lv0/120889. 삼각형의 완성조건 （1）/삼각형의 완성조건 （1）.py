9e8b6684cf530538f38be95b67de8611edf60014
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-05-06 02:38:58 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,7 +1,2 @@
 def solution(sides):
-    if sides[0] + sides[1] > sides[2] \
-        and sides[0] + sides[2] > sides[1] \
-        and sides[1] + sides[2] > sides[0]:
-        return 1
-    else:
-        return 2
+    return 1 if max(sides) < sum(sides) - max(sides) else 2
