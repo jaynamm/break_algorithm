042bee87def269f99cceff359624313fa6b404d0
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-05-06 02:06:09 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 33.23 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -2,17 +2,13 @@ def solution(s):
     answer = [-1]
     
     for i in range(1, len(s)):
-        cnt = 0
         check = False
         
         for j in range(i-1, -1, -1):
             if s[i] == s[j]:
                 check = True
                 answer.append(i-j)
-                cnt = 0
                 break
-            else:
-                cnt += 1
         if check == False:
             answer.append(-1)
     
