89e43302ef88ac0e69f7445acb4430ef949a6b63
Jaynam <48994100+jaynamm@users.noreply.github.com>
2023-05-06 01:52:20 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,7 +1,2 @@
 def solution(n):
-    answer = 0
-    if round(n**(0.5))**2 == n:
-        answer = (n**(0.5)+1)**2
-    else:
-        answer = -1
-    return answer
+    return (n**(0.5)+1)**2 if round(n**(0.5))**2 == n else -1
