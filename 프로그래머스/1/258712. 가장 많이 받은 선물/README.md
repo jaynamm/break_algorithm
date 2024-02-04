4fe8986b0ee2499dde74bd5739629d702257b680
Jaynam <48994100+jaynamm@users.noreply.github.com>
2024-02-04 23:11:01 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 8.22 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 10.70 ms
+메모리: 10.3 MB, 시간: 8.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 23:30:39
+2024년 2월 0일 23:10:57
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,48 +1,66 @@
+# 1) 선물을 더 많이 준 사람이 선물 +1 받음
+# 2) 선물 받은 기록 없음 or 같음 -> 선물지수가 더 큰 사람에게 +1 받음, 만약 선물지수가 같다면 없음
+
 def solution(friends, gifts):
     answer = 0
-    length = len(friends)
     
-    # Create Table - 선물을 준 횟수
+    # 친구 수
+    length = len(friends)
+
+    # 선물을 주고받은 데이터를 저장할 2중 배열 생성
     give = [[0] * length for _ in range(length)]
     
     for gift in gifts:
+        # 선물을 준 사람과 받은 사람을 가져온다.
         giver, taker = gift.split()
-
+        
+        # 준 사람과 받은 사람의 인덱스를 가져온다.
         give_id = friends.index(giver)
         take_id = friends.index(taker)
-
-        give[give_id][take_id] += 1
         
-    print(give)
+        # 배열에 추가한다.
+        give[give_id][take_id] += 1
     
-    # Calculate
-    score = [0] * len(friends)
-
+    result = [0] * length
+    
     for i in range(length):
         for j in range(i+1, length):
-            give_score = give[i][j] # i 가 j 에게 준 선물 개수
-            take_score = give[j][i] # j 가 i 에게 준 선물 개수
+            # i 가 j 에게 준 선물 개수
+            give_score = give[i][j] 
+            # j 가 i 에게 준 선물 개수 = i 가 j 에게 받은 선물 개수
+            take_score = give[j][i] 
             
-            if (give_score != take_score) and (give_score > 0 or take_score > 0):
+            # 조건 1: 주고받은 선물의 개수가 같지 않고 한 명의 친구가 선물을 준 경우
+            if (give_score != take_score) and (give_score > 0 or  take_score > 0):
+                # 조건 1-1: 선물을 더 많이 준 친구가 선물 하나를 받는다.
                 if give_score > take_score:
-                    score[i] += 1
+                    result[i] += 1
                 else:
-                    score[j] += 1
+                    result[j] += 1
+            # 조건 2: 주고받은 선물이 없는 경우
             else:
+                # 조건 2-1: 선물지수가 높은 친구가 선물 하나를 받는다.
+                # 준 선물의 수 = 행(row)의 합
+                # 받은 선물의 수 = 열(column)의 합
+                
+                # i 가 준 선물 개수
                 give_sum = sum(give[i])
+                # j 가 준 선물 개수
                 take_sum = sum(give[j])
                 
+                # 선물 지수를 구한다.
+                # 선물 지수 = 준 선물 - 받은 선물
                 for k in range(length):
                     give_sum -= give[k][i]
                     take_sum -= give[k][j]
-                
-                if give_sum > take_sum:
-                    score[i] += 1
-                elif give_sum < take_sum:
-                    score[j] += 1
                     
-    print(score)
-    
-    answer = max(score)
-    
+                # 선물지수 비교
+                if give_sum > take_sum:
+                    result[i] += 1
+                elif give_sum < take_sum:
+                    result[j] += 1
+                    
+    # 선물을 가장 많이 받는 친구의 선물 개수를 구한다.
+    answer = max(result)
+                
     return answer
