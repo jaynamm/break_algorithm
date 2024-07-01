39f211f0510552db4ae3e7dbd50c83fc61cda080
Jaynam <48994100+jaynamm@users.noreply.github.com>
2024-07-01 10:52:57 +0900

    [Bronze I] Title: 2진수 8진수, Time: 44 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.py
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.py
@@ -1,5 +1,2 @@
 import sys
-
-binary_number = str(sys.stdin.readline().strip())
-decimal_number = int(binary_number, 2)
-print("{0:o}".format(decimal_number))
+print("{0:o}".format(int(str(sys.stdin.readline().strip()), 2)))
