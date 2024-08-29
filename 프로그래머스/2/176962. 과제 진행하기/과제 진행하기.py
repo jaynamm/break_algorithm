223aed205ac59140ec827c0e30f2b4b97b4bc460
Jaynam <48994100+jaynamm@users.noreply.github.com>
2024-08-29 23:50:25 +0900

    [level 2] Title: 과제 진행하기, Time: 2.36 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,32 +1,34 @@
 def solution(plans):
-    result = []
+    answer = []
     # 입력 데이터 전처리: 시작 시간 기준으로 정렬
     plans.sort(key=lambda x: x[1])
     
-    stack = []  # 진행 중인 일정들을 저장할 스택
+    running = []  # 진행 중인 일정들을 저장할 스택
     current_time = 0  # 현재 시간
     
     for plan in plans:
-        name, start_time, duration = plan
-        start_time = int(start_time[:2]) * 60 + int(start_time[3:])  # 시간을 분으로 변환
-        duration = int(duration)
+        name, start, playtime = plan
+        
+        # 시간을 분으로 변환
+        start = int(start[:2]) * 60 + int(start[3:])  
+        playtime = int(playtime)
         
         # 스택에 있는 일정 처리
-        while stack and current_time + stack[-1][1] <= start_time:
-            last_name, last_duration = stack.pop()
-            current_time += last_duration
-            result.append(last_name)
+        while running and current_time + running[-1][1] <= start:
+            last_name, last_playtime = running.pop()
+            current_time += last_playtime
+            answer.append(last_name)
         
         # 스택에 남은 일정의 남은 시간을 업데이트
-        if stack:
-            stack[-1][1] -= (start_time - current_time)
+        if running:
+            running[-1][1] -= (start - current_time)
         
         # 새로운 일정 스택에 추가
-        stack.append([name, duration])
-        current_time = start_time
+        running.append([name, playtime])
+        current_time = start
     
     # 스택에 남은 일정 처리
-    while stack:
-        result.append(stack.pop()[0])
+    while running:
+        answer.append(running.pop()[0])
     
-    return result
+    return answer
