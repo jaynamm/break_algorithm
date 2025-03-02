9221fe2a8d0e2cb9fedc6278e387f08c0a9802b4
Jaynam <48994100+jaynamm@users.noreply.github.com>
2025-03-02 21:29:33 +0900

    [level 1] Title: 유연근무제, Time: 1.47 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -4,43 +4,34 @@ def solution(schedules, timelogs, startday):
     # 일한 직원의 수
     workers = len(schedules)
     
-    # 체크할 요일 확인
-    weeks = [True] * 7
-    
-    # 토요일 일요일 제외
-    for day in range(0, 7):
-        now_day = startday + day
-        
-        if now_day > 7:
-            now_day -= 7
+    # 요일 계산
+    day_of_weeks = [(startday + i - 1) % 7 + 1 for i in range(0, 7)]
+    print(day_of_weeks)
             
-        if now_day % 6 == 0 or now_day % 7 == 0:
-            weeks[day] = False
-            
+    # 각 직원마다 출근 여부 확인
     for i in range(workers):
-        cutline = schedules[i] + 10
-        
-        if cutline % 100 == 60:
-            cutline = ((cutline // 100) + 1) * 100
-        elif cutline % 100 > 60:
-            hour = (cutline // 100) + 1
-            minute = (cutline % 100) % 60
-            cutline = (hour * 100) + minute
-            
+        # 성공 여부
         success = True
         
-        if schedules[i] > 1100:
-            success = False
-            break
+        # 출근 데드라인
+        deadline = schedules[i] + 10
         
-        for j in range(len(timelogs[i])):    
-            if weeks[j] == False:
+        # 만약 10분을 더한 시각이 60분을 넘어가면 + 1시간을 해주어야 한다.
+        if deadline % 100 > 59:
+            deadline += 100
+            deadline -= 60
+        
+        # 각 직원마다 시간 기록을 가져온다.
+        for j in range(len(timelogs[i])):
+            # 만약 토요일(6), 일요일(7) 이라면 넘어간다.
+            if day_of_weeks[j] in [6, 7]:
                 continue
             else:
-                if timelogs[i][j] > cutline:
+                # deadline 보다 늦었다면 이벤트 실패
+                if timelogs[i][j] > deadline:
                     success = False
                     break
-        
+        # 성공하면 상품을 받을 수 있다.        
         if success:
             answer += 1
     
