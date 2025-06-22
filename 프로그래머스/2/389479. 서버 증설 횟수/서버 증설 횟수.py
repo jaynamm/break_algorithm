84bd802e677cfa50eec93a404d6e944cffc86158
Jaynam <48994100+jaynamm@users.noreply.github.com>
2025-06-23 00:17:49 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.04 ms, Memory: 8.98 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -12,14 +12,17 @@ def solution(players, m, k):
             # 증설이 필요한 서버 수 = 필요한 서버 수 - 현재 서버 수
             need = server[i][0] - server[i][1]
             
-            # 서버 증설 횟수 증가
+            # 서버 증설 횟수 증가시킨다.
             server[i][2] += need
             
+            # 서버를 증설할 경우 최대 24시간을 넘기지 않도록 한다.
             end = min(i + k, len(server))
             
+            # k 시간만큼 서버를 증설해준다.
             for j in range(i, end):
                 server[j][1] += need
     
+    # 서버를 증설한 횟수를 모두 더해준다.
     answer = sum([s[2] for s in server])
     
     return answer
